c70a174eac231641eda14c785a151228a1543e09
amw <amw@amwtekiMacBook-Pro.local>
2013-04-18 20:24:31 +0000

    ädd file

---
+++ ffmpeg_and_sdl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ffmpeg_and_sdl
+//
+//  Created by amw on 13-4-18.
+//  Copyright (c) 2013年 amw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ffmpeg_and_sdl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ffmpeg_and_sdl
+//
+//  Created by amw on 13-4-18.
+//  Copyright (c) 2013年 amw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
