0ff7d62303bf4f975a797d6418c364dcd2befdef
amw <amw@amwtekiMacBook-Pro.local>
2013-04-21 20:51:34 +0000

    add video and audio and saveppm

---
+++ ffmpeg_and_sdl/KxAudioFrame.h
@@ -0,0 +1,13 @@
+//
+//  KxAudioFrame.h
+//  ffmpeg_and_sdl
+//
+//  Created by amw on 13-4-21.
+//  Copyright (c) 2013年 amw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KxAudioFrame : NSObject
+
+@end
---
+++ ffmpeg_and_sdl/KxAudioFrame.m
@@ -0,0 +1,13 @@
+//
+//  KxAudioFrame.m
+//  ffmpeg_and_sdl
+//
+//  Created by amw on 13-4-21.
+//  Copyright (c) 2013年 amw. All rights reserved.
+//
+
+#import "KxAudioFrame.h"
+
+@implementation KxAudioFrame
+
+@end
